a005c56659610232798ed6ae56d4ad7644bd5feb
Akash4889 <akashsherikar48@gmail.com>
2022-08-16 14:50:03 +0530

    Calculating WagesEmployee wage problem

---
+++ src/bridgelabz/employeewage.java
@@ -0,0 +1,34 @@
+package bridgelabz;
+
+public class employeewage {
+        public static void main(String[] args){
+
+            int wagePerHour=20;
+            int fullTimeHour=8;
+            int partTimeHour=4;
+            int workingDays=20;
+            int dailyWage=0;
+
+            for (int day = 0; day < workingDays; day++){
+
+                int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
+
+                switch (employeeCheck){
+                    case 1:
+                        dailyWage = dailyWage + fullTimeHour * wagePerHour;
+                        System.out.println("Employee is present full time: " + dailyWage);
+                        break;
+
+                    case 2:
+                        dailyWage = dailyWage + partTimeHour * wagePerHour;
+                        System.out.println("Employee is present part time: " + dailyWage);
+                        break;
+
+                    default:
+                        System.out.println("No wages: " + dailyWage);
+                }
+                System.out.println("Total wage of month: " + dailyWage);
+            }
+        }
+    }
+
